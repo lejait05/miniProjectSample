76723741b0dcd2b218a6990aeaad201cc5f486e7
lineisha gibson <lineishagibson@gmail.com>
2022-11-13 17:05:27 -0600

    .

---
+++ src/main/java/mini/miniProjectSampleApplication.java
@@ -0,0 +1,11 @@
+package mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class miniProjectSampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(miniProjectSampleApplication.class, args);
+    }
+}
